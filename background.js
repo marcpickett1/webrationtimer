0433b8187cccc60186edf9ebc2e49e74c29a5d08
Marctar the Mad <marcpickett1@gmail.com>
2017-04-27 20:20:56 -0400

    Foo.

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ loadRingIfNecessary();
 
 function defaultPrefs() {
   return {
-    siteList: ['marcpickett1.github.io', '127.0.0.1', 'marcpickett.com'],
+    siteList: ['marcpickett1.github.io', '127.0.0.1:4000', 'marcpickett.com'],
     duration: 25 * 60,
     shouldRing: true}}
 function loadPrefs() {
@@ -23,8 +23,7 @@ function setPrefs(prefs) {
   return prefs;
 }
 function loadRingIfNecessary () {
-  // if(PREFS.shouldRing && !ringLoaded) {
-  if(true && !ringLoaded) {
+  if(!ringLoaded) {
     RING.onload = function () {console.log('ring loaded'); ringLoaded = true;}
     RING.load();
   }}
@@ -49,7 +48,7 @@ function Pomodoro(options) {
     this.currentTimer = new Pomodoro.Timer(this, timerOptions);
     this.currentTimer.start();
   }
-  this.restart = function () {if(this.currentTimer) {this.currentTimer.restart();}}
+  this.stop = function () {if(this.running) {this.currentTimer.stop();}}
 }
 
 Pomodoro.Timer = function Timer(pomodoro, options) {
@@ -61,9 +60,8 @@ Pomodoro.Timer = function Timer(pomodoro, options) {
     options.onStart(timer);
     options.onTick(timer);
   }
-  this.restart = function() {
-    this.timeRemaining = options.duration;
-    options.onTick(timer);
+  this.stop = function() {
+    this.timeRemaining = 0;
   }
   this.timeRemainingString = function () {
     if ((this.timeRemaining % 60) > 9) {
@@ -110,43 +108,28 @@ function executeInAllBlockedTabs(action) {
 
 //
 var notification, mainPomodoro = new Pomodoro({
-  // getDuration: function () { return PREFS.duration },
   getDuration: function () { return 25 * 60 },
-  timer: {onEnd: function (timer) {
-    chrome.browserAction.setIcon({path: ICONS.ACTION.CURRENT['offline']});
-    chrome.browserAction.setBadgeText({text: ''});
-    // if(PREFS.showNotifications) {
-    if(false) {
-      chrome.notifications.create("", {
-        type: "basic",
-        // title: chrome.i18n.getMessage("timer_end_notification_header"),
-        // message: chrome.i18n.getMessage("timer_end_notification_body", "Time's up: You're now offline."),
-        title: "Offline, yo",
-        message: "Time's up: You're now offline.",
-        priority: 2,
-        iconUrl: ICONS.FULL['offline']
-      }, function() {});
-    }
-    // if(PREFS.shouldRing) {console.log("playing ring", RING); RING.play();}
-    if(true) {console.log("playing ring", RING); RING.play();}
-  }, onStart: function (timer) {
-    chrome.browserAction.setIcon({path: ICONS.ACTION.CURRENT['online']});
-    chrome.browserAction.setBadgeBackgroundColor({color: [192, 0, 0, 255]});
-    executeInAllBlockedTabs('unblock');
-    if(notification) notification.cancel();
-    var tabViews = chrome.extension.getViews({type: 'tab'}), tab;
-    for(var i in tabViews) {
-      tab = tabViews[i];
-      if(typeof tab.startCallbacks !== 'undefined') {tab.startCallbacks['online']();}
-    }}, onTick: function (timer) {chrome.browserAction.setBadgeText({text: timer.timeRemainingString()});}
-  }});
+  timer: {
+    onEnd: function (timer) {
+      chrome.browserAction.setIcon({path: ICONS.ACTION.CURRENT['offline']});
+      chrome.browserAction.setBadgeText({text: ''});
+      console.log("playing ring", RING); RING.play();
+      executeInAllBlockedTabs('block');},
+    onStart: function (timer) {
+      chrome.browserAction.setIcon({path: ICONS.ACTION.CURRENT['online']});
+      chrome.browserAction.setBadgeBackgroundColor({color: [192, 0, 0, 255]});
+      executeInAllBlockedTabs('unblock');
+      var tabViews = chrome.extension.getViews({type: 'tab'}), tab;
+      for(var i in tabViews) {
+	tab = tabViews[i];
+	if(typeof tab.startCallbacks !== 'undefined') {tab.startCallbacks['online']();}}},
+    onTick: function (timer) {chrome.browserAction.setBadgeText({text: timer.timeRemainingString()});}}});
+
+executeInAllBlockedTabs('block');
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if(!mainPomodoro.running) {mainPomodoro.start();}});
+  if(!mainPomodoro.running) {mainPomodoro.start();}
+  else {mainPomodoro.stop();}});
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if(!mainPomodoro.running) {executeInTabIfBlocked('block', tab);}});
-
-// Clicking the notification brings you back to Chrome, in whatever window you were last using.
-chrome.notifications.onClicked.addListener(function (id) {
-  chrome.windows.getLastFocused(function (window) {chrome.windows.update(window.id, {focused: true});});});
